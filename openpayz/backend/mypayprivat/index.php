40ce82d84dc05d2d5e387a00a6e80c94c98642c5
Pautina <alex.chernyk@gmail.com>
2017-04-26 22:02:06 +0300

    Удаление Opanpayz

--- openpayz/backend/mypayprivat/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//config payment URL here.
-$url='https://my-payments.privatbank.ua/mypayments/customauth/identification/fp/static?staticToken=123456789';
-print('<script language="javascript">document.location.href="' . $url . '";</script>'); 
-
-?>
