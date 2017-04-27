374322a8468302ad8ce1c66c2f78daee5b324a62
Pautina <alex.chernyk@gmail.com>
2017-04-27 16:06:42 +0300

    Fix gramma mistake for function use MySQLi driver

--- modules/general/adservice/index.php
+++ modules/general/adservice/index.php
@@ -70,7 +70,7 @@ if ($us_config['ADSERVICE_ENABLED']) {
         if (!empty($availableServices)) {
             foreach ($availableServices as $eachService) {
                 $eachData = explode(":", $eachService);
-                $serviceTagID[] = isset($eachData[1]);
+                $serviceTagID[] = $eachData[1];
             }
             foreach ($serviceTagID as $eachTagID) {
                 reset($serviceTagID);
@@ -136,7 +136,7 @@ if ($us_config['ADSERVICE_ENABLED']) {
                 $eq = false;
                 $eachData = explode(":", $eachService);
                 $serviceName = $eachData[0];
-                $serviceTagID = isset($eachData[1]);
+                $serviceTagID = $eachData[1];
                 if (!empty($allSheduledl)) {
                     foreach ($allSheduled as $eachShedule) {
                         if ($eachShedule['param'] === $serviceTagID) {
